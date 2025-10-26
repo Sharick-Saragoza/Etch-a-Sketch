be9cfd61f58022c80aeefeea36a50bff69b84719
Sharick <150659105+Sharick-Saragoza@users.noreply.github.com>
2025-10-26 18:13:21 +0100

    You're able to draw using you're mouse. And the item classes have been changed to id to access the styling.

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container"); 
 
 function createGrid(amount) {
     
@@ -12,10 +12,16 @@ function createGrid(amount) {
         for (let i = 0; i <= amount; i++) {
             // Creates AMOUNT of items inside ONE row. 
             const newItem = document.createElement("div");
-            newItem.className = "item";
+            newItem.setAttribute("id", "item");
+
+            // Makes the item black when you hover over it.
+            newItem.addEventListener("mouseover", () => {
+                newItem.style.backgroundColor = "black";
+            });
+
             newRow.appendChild(newItem);
         }
     }
 }
 
-createGrid(16);
+createGrid(32);
