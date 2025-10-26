233d3b3a373fee82865ca00bfead3d695f05aadd
Sharick <150659105+Sharick-Saragoza@users.noreply.github.com>
2025-10-26 16:28:05 +0100

    Grid can now be created with the createGrid function by giving it an amount by choice.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
-console.log("Script is working!");
+const container = document.querySelector(".container");
+
+function createGrid(amount) {
+    
+    for (let i = 0; i <= amount; i++) {
+        // Creates AMOUNT of rows.
+        const newRow = document.createElement("div");
+        newRow.className = "row";
+        const row = document.querySelector(".row");
+        container.appendChild(newRow);
+
+        for (let i = 0; i <= amount; i++) {
+            // Creates AMOUNT of items inside ONE row. 
+            const newItem = document.createElement("div");
+            newItem.className = "item";
+            newRow.appendChild(newItem);
+        }
+    }
+}
+
+createGrid(16);
